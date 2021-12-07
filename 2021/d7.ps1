e57c8799f1c60db0494273d4c12c493c1c7f2a4a
Ryan Bolger <ryan-oss@xyto.cc>
2021-12-06 22:48:20 -0800

    day 7 significantly sped up with a cache and parallel foreach

--- 2021/d7.ps1
+++ 2021/d7.ps1
@@ -2,7 +2,9 @@
 
 [CmdletBinding()]
 param(
-    $InputFile = '.\d7.txt'
+    $InputFile = '.\d7.txt',
+    [switch]$NoPart1,
+    [switch]$NoPart2
 )
 
 # https://adventofcode.com/2021/day/7
@@ -12,27 +14,41 @@ Set-Clipboard $data
 
 $pos = [int[]]((gcb) -split ',') | Sort-Object
 
-$median = if ($pos.Count % 2) {
-    $pos[[Math]::Floor($pos.Count/2)]
-} else {
-    $pos[$pos.Count/2],$pos[$pos.Count/2-1] | measure -average | % average
-}
-Write-Verbose "median = $median"
+# Part 1
+if (-not $NoPart1) {
 
-$pos | %{ [Math]::Abs($_ - $median) } | measure -sum | % sum
-
-
-$pos[0]..$pos[-1] | %{
-    $p = $_
-    #Write-Verbose "`$p = $p"
-    $fuel = $pos | %{
-        $delta = [Math]::Abs($_ - $p)
-        $f = (1..$delta | %{"$_"}) -join '+' | iex
-        $f
-        #Write-Verbose "    $_ - $p = $f"
-    } | measure -sum | % sum
-    [pscustomobject]@{
-        pos = $p
-        fuel = $fuel
+    $median = if ($pos.Count % 2) {
+        $pos[[Math]::Floor($pos.Count/2)]
+    } else {
+        $pos[$pos.Count/2],$pos[$pos.Count/2-1] | measure -average | % average
     }
-} | sort fuel | select -first 1
+    Write-Verbose "median = $median"
+
+    $pos | %{ [Math]::Abs($_ - $median) } | measure -sum | % sum
+
+}
+
+# Part 2
+if (-not $NoPart2) {
+
+    # lookup table for distance costs
+    $costs = @{}
+    0..$pos[-1] | %{
+        $total += $_
+        $costs[$_] = $total
+    }
+
+    # a bit of parallel processing to brute force all of the possibilities
+    $pos[0]..$pos[-1] | ForEach-Object -Parallel {
+        $p = $_
+        $fuel = $using:pos | %{
+            $delta = [Math]::Abs($_ - $p)
+            ($using:costs)[$delta]
+        } | measure -sum | % sum
+        [pscustomobject]@{
+            pos = $p
+            fuel = $fuel
+        }
+    } | sort fuel | select -first 1 | select -expand fuel
+
+}
