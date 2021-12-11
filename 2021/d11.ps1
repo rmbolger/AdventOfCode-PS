1d0231e7b314ab61d2dc2335510fcf2e6ee9abd4
Ryan Bolger <ryan-oss@xyto.cc>
2021-12-11 12:14:37 -0800

    added day 11 visualization

--- 2021/d11.ps1
+++ 2021/d11.ps1
@@ -2,7 +2,8 @@
 
 [CmdletBinding()]
 param(
-    $InputFile = '.\d11.txt'
+    $InputFile = '.\d11.txt',
+    [switch]$ShowViz
 )
 
 # https://adventofcode.com/2021/day/11
@@ -11,8 +12,8 @@ $data = Get-Content $InputFile
 Set-Clipboard $data
 
 # instead of organizing the grid into a 2D array, we're going to encode
-# the x,y coords into a single 8-bit integer by -shl'ing the x 4 bits
-# to the left and -bor'ing the y.
+# the x,y coords into a single 8-bit integer by -shl'ing the y 4 bits
+# to the left and -bor'ing the x.
 
     $octEnergy = [Collections.Generic.Dictionary[int,int]]::new(200)
     $octNbrs = [Collections.Generic.Dictionary[int,int[]]]::new(200)
@@ -32,7 +33,7 @@ Set-Clipboard $data
     $lines = Get-Clipboard
     for ($y=0; $y -lt $lines.Count; $y++) {
         for ($x=0; $x -lt $lines[0].Length; $x++) {
-            $xy = $x -shl 4 -bor $y
+            $xy = $y -shl 4 -bor $x
             $val = [int]$lines[$y][$x]-48
             if (-not ($octEnergy.ContainsKey($xy))) {
                 $octEnergy.$xy = $val
@@ -47,7 +48,7 @@ Set-Clipboard $data
                 if ($dx -lt 0 -or $dx -gt 9) { continue }
                 if ($dy -lt 0 -or $dy -gt 9) { continue }
 
-                $dxy = $dx -shl 4 -bor $dy
+                $dxy = $dy -shl 4 -bor $dx
                 # add the energy now if it's not already there
                 if (-not ($octEnergy.ContainsKey($dxy))) {
                     $octEnergy.$dxy = [int]$lines[$dy][$dx]-48
@@ -60,23 +61,48 @@ Set-Clipboard $data
         }
     }
 
-function Out-OctoGrid {
-    [CmdletBinding()]
-    param(
-        [Collections.Generic.Dictionary[int,int]]$grid
-    )
-    for ($y=0; $y -lt $lines.Count; $y++) {
-        $chars = for ($x=0; $x -lt $lines[0].Length; $x++) {
-            $xy = $x -shl 4 -bor $y
-
-            $grid.$xy.ToString().PadLeft(3)
-        }
-        $chars -join ''
+    $colorVal = @{
+        0 = 0x00305f
+        1 = 0x104478
+        2 = 0x2c578d
+        3 = 0x3d659d
+        4 = 0x5379b2
+        5 = 0x698ec9
+        6 = 0x7ea3df
+        7 = 0x94b8f6
+        8 = 0xabceff
+        9 = 0xffffff
     }
-    ''
-}
+    #10..20 | %{ $colorVal.$_ = 0xc1e5ff }  # post-flash slightly less
+    10..20 | %{ $colorVal.$_ = 0x00305f }   # post-flash immediately dark
+
+    function Out-OctoGrid {
+        [CmdletBinding()]
+        param(
+            [Parameter(Mandatory,ValueFromPipeline)]
+            [Collections.Generic.Dictionary[int,int]]$grid,
+            [int]$DelayMs=50
+        )
+
+        Process {
+            if (-not $ShowViz) { return }
+            # Ʊ - octo char
+            Clear-Host
+            ''
+            for ($y=0; $y -lt $lines.Count; $y++) {
+                $chars = for ($x=0; $x -lt $lines[0].Length; $x++) {
+                    $xy = $y -shl 4 -bor $x
+                    " $($PSStyle.Foreground.FromRgb($colorVal[$grid.$xy]))Ʊ"
+                }
+                $chars -join ''
+            }
+            $PSStyle.Reset
+            Start-Sleep -Milliseconds $DelayMs
+        }
+    }
 
     $flashed = [Collections.Generic.List[int]]::new(200)
+    Out-OctoGrid $octEnergy
 
     while ($true) {
         $i += 1
@@ -86,6 +112,7 @@ function Out-OctoGrid {
         foreach ($key in $octEnergy.Keys) {
             $octEnergy.$key += 1
         }
+        $octEnergy | Out-OctoGrid
 
         $flashed.Clear()
         while ($flashing = ($octEnergy.GetEnumerator() | ?{ $_.Value -gt 9 -and $_.Key -notin $flashed})) {
@@ -98,24 +125,20 @@ function Out-OctoGrid {
                     $octEnergy.$nbr += 1
                 }
             }
+            $octEnergy | Out-OctoGrid
         }
 
         # count and zero out the flashed
-        $totalFlashed += $flashed.Count
+        if ($i -le 100) { $totalFlashed += $flashed.Count }
         foreach ($xy in $flashed) {
             $octEnergy[$xy] = 0
         }
-
-        # Output Part 1 when we get there
-        if ($i -eq 100) {
-            Write-Host "$totalFlashed total flashed after 100 steps"
-        }
+        $octEnergy | Out-OctoGrid
 
         # check for Part 2 completion
         if ($flashed.Count -eq 100) {
-            Write-Host "All flashed on step $i"
             break
         }
-
-        #Out-OctoGrid $octEnergy
     }
+    Write-Host "$totalFlashed total flashed after 100 steps"
+    Write-Host "All flashed on step $i"
