bd8aa38a2ec23d1c53e21c8a41c2bbddc67b740c
Ryan Bolger <ryan-oss@xyto.cc>
2022-12-10 16:39:45 -0800

    day 10 cleaned up

--- 2022/d10.ps1
+++ 2022/d10.ps1
@@ -7,77 +7,65 @@ param(
 
 # https://adventofcode.com/2022/day/10
 
-$cycle = 1
-[int]$x = 1
-$sprite = 0,1,2
-$crt = @(
-    '                                        '.ToCharArray(),
-    '                                        '.ToCharArray(),
-    '                                        '.ToCharArray(),
-    '                                        '.ToCharArray(),
-    '                                        '.ToCharArray(),
-    '                                        '.ToCharArray()
-)
-$crtLine = 0
+class EPhone {
+    [int]$cycle = 1
+    [int]$x = 1
+    [int[]]$sprite = 0,1,2
+    [char[][]]$crt = @(
+        '                                        '.ToCharArray(),
+        '                                        '.ToCharArray(),
+        '                                        '.ToCharArray(),
+        '                                        '.ToCharArray(),
+        '                                        '.ToCharArray(),
+        '                                        '.ToCharArray()
+    )
+    [int]$crtLine = 0
+    [int]$signalSum = 0
 
-$strengths = switch -Regex (Get-Content $InputFile) {
+    [void]DoCycle([int]$Add) {
 
-    'noop' {
-        if (($cycle-1)%40 -in $sprite) {
-            $crt[$crtLine][($cycle-1)%40] = '#'
+        # draw the next pixel
+        if (($this.cycle-1)%40 -in $this.sprite) {
+            $this.crt[$this.crtLine][($this.cycle-1)%40] = '#'
         }
-        #Write-Verbose "c$cycle x$x noop - $($crt[$crtLine] -join '') - row $crtLine"
+        #Write-Verbose "c$($this.cycle) x$($this.x) $Add - $($this.crt[$this.crtLine] -join '') - row $($this.crtLine)"
 
-        if ($cycle%40 -eq 0) {
-            $crtLine++
-            #Write-Verbose "c$cycle crt$crtLine"
-        }
-        $cycle++
-        if (($cycle-20)%40 -eq 0) {
-            $cycle * $x
-            #Write-Verbose "c$cycle * x$x = $($cycle*$x)"
+        # If we have a value, add it and move the sprite
+        if ($Add -ne 0) {
+            $this.x += $Add
+            $this.sprite = ($this.x-1),$this.x,($this.x+1)
         }
 
-    }
-    'addx (\S+)' {
-        $addx = $matches[1]
-
-        if (($cycle-1)%40 -in $sprite) {
-            $crt[$crtLine][($cycle-1)%40] = '#'
-        }
-        #Write-Verbose "c$cycle x$x $addx - $($crt[$crtLine] -join '') - row $crtLine"
-
-        if ($cycle%40 -eq 0) {
-            $crtLine++
-            #Write-Verbose "c$cycle crt$crtLine"
-        }
-        $cycle++
-        if (($cycle-20)%40 -eq 0) {
-            $cycle * $x
-            #Write-Verbose "c$cycle * x$x = $($cycle*$x)"
+        # move to next CRT line if necessary
+        if ($this.cycle%40 -eq 0) {
+            $this.crtLine++
+            #Write-Verbose "c$($this.cycle) crt$($this.crtLine)"
         }
 
-        if (($cycle-1)%40 -in $sprite) {
-            $crt[$crtLine][($cycle-1)%40] = '#'
-        }
-        #Write-Verbose "c$cycle x$x $addx - $($crt[$crtLine] -join '') - row $crtLine"
+        $this.cycle++
 
-        $x += $addx
-        $sprite = ($x-1),$x,($x+1)
+        # emit Part 1 signal strength
+        if (($this.cycle-20)%40 -eq 0) {
+            $this.signalSum += $this.cycle * $this.x
+            #Write-Verbose "c$($this.cycle) * x$($this.x) = $($this.cycle*$this.x)"
+        }
 
-        if ($cycle%40 -eq 0) {
-            $crtLine++
-            #Write-Verbose "c$cycle crt$crtLine"
-        }
-        $cycle++
-        if (($cycle-20)%40 -eq 0) {
-            $cycle * $x
-            #Write-Verbose "c$cycle * x$x = $($cycle*$x)"
-        }
     }
 }
-$strengths | Measure-Object -Sum
-| Select-Object -Expand Sum
+
+$phone = [EPhone]::new()
+
+switch -Regex (Get-Content $InputFile) {
+    'noop' {
+        $phone.DoCycle(0)
+    }
+    'addx (\S+)' {
+        #$addx = $matches[1]
+        $phone.DoCycle(0)
+        $phone.DoCycle($matches[1])
+    }
+}
+$phone.signalSum
 
 # Part 2 Output
-$crt | ForEach-Object { $_ -join '' }
+$phone.crt | ForEach-Object { $_ -join '' }
